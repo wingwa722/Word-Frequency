b010a823858040124e0f403efcfc6b05f3b6b587
Chan Wing Wa <wing@ChandeMacBook-Pro.local>
2021-10-25 16:23:37 +0800

    [WordFrequency][Wing]Refactor: Rename the word class

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,7 +1,8 @@
+//rename: Input -> Word
 public class Input {
     private String value;
     private int count;
-
+    //rename: value -> word
     public Input(String w, int i){
         this.value =w;
         this.count =i;
